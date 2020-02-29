92c7f56db95200b84eb02c07bdab881ea893fb65
Alice <alice.leon10@gmail.com>
2020-02-29 13:04:52 +0100

    [Migration] changement du type de champ de sess_lifetime en BIGINT

--- src/Migrations/Version20200228222747.php
+++ src/Migrations/Version20200228222747.php
@@ -21,7 +21,7 @@ final class Version20200228222747 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE sessions (sess_id VARCHAR(128) NOT NULL PRIMARY KEY, sess_data BLOB NOT NULL, sess_time INTEGER UNSIGNED NOT NULL, sess_lifetime MEDIUMINT NOT NULL) COLLATE utf8_bin, ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sessions (sess_id VARCHAR(128) NOT NULL PRIMARY KEY, sess_data BLOB NOT NULL, sess_time INTEGER UNSIGNED NOT NULL, sess_lifetime BIGINT NOT NULL) COLLATE utf8_bin, ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void

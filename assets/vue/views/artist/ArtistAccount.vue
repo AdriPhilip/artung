2c4d223fe0533e2412ed4747d60c9125acae7633
Audie <audie.menut@gmail.com>
2020-02-29 15:58:43 +0100

    view ArtistAccount

--- assets/vue/views/artist/ArtistAccount.vue
+++ assets/vue/views/artist/ArtistAccount.vue
@@ -1,11 +1,84 @@
 <template>
   <div>
     <Header />
-    <FormGroup />
-    <EditIcon />
-    <FormGroupInline />
+
+    <!-- Formulaire de données de profil -->
+    <form @submit.prevent>
+      <EditIcon />
+      <FormGroup
+        form-group="photoInput"
+        type-status="text"
+        text="Photo de profil"
+      />
+      <div class="form-group">
+        <label for="category">Catégorie</label>
+        <select
+          id="category"
+          class="form-control"
+        >
+          <option value="architecture">
+            Architecture
+          </option>
+          <option value="sculpture">
+            Sculpture
+          </option>
+          <option value="visual">
+            Arts visuels
+          </option>
+          <option value="music">
+            Musique
+          </option>
+          <option value="literature">
+            Littérature
+          </option>
+          <option value="performing">
+            Arts de la scène
+          </option>
+          <option value="film">
+            Cinéma
+          </option>
+        </select>
+        <div class="form-group">
+          <label for="description">Description</label>
+          <textarea
+            id="description"
+            class="form-control"
+            rows="3"
+          />
+        </div>
+      </div>
+    </form>
+
+    <hr>
+
+    <!-- Paramètrer mes flux -->
+    <span>Paramétrer mes flux</span>
+    <form @submit.prevent>
+      <EditIcon />
+      <FormGroupInline icon-status="facebook" />
+      <FormGroupInline icon-status="twitter" />
+      <FormGroupInline icon-status="youtube" />
+      <FormGroupInline icon-status="wordpress" />
+    </form>
+
+    <hr>
+
+    <!-- Formulaire de données de connexion -->
     <FormAccount />
-    <TextButton />
+
+    <hr>
+
+    <!-- Boutons se déconnecter et supprimer mon profil -->
+    <TextButton
+      text="Me déconnecter"
+      secondary
+      @onClick="routerPush('Home', '')"
+    />
+    <TextButton
+      text="Supprimer mon profil"
+      secondary
+      @onClick="routerPush('DeleteAccount', '')"
+    />
   </div>
 </template>
 
@@ -31,5 +104,9 @@ export default {
 </script>
 
 <style lang="scss">
-
+hr {
+  height: var(--spacing-xs);
+  background-color: var(--dark);
+  border-top: 0;
+}
 </style>

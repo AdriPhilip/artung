f1b4e0a303a97de561e90a68d16f252c2b1e6bcd
Alice <alice.leon10@gmail.com>
2020-02-29 18:23:55 +0100

    Modif pour rester authentifié si on recharge la page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,9 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Security\Core\Security;
 
 /**
  * @Route("/api")
